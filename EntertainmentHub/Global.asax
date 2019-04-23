95c13dd890c1ba53b7ec81b04fbc8c4aee33018e
BrianHwang888 <bh1531@nyu.edu>
2019-04-23 19:31:51 -0400

    Added System.Web.MVC reference

--- EntertainmentHub/Global.asax
+++ EntertainmentHub/Global.asax
@@ -1,1 +1,1 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="WebApplication1.MvcApplication" Language="C#" %>
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="EntertainmentHub.Global" Language="C#" %>
--- EntertainmentHub/Global.asax.cs
+++ EntertainmentHub/Global.asax.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -6,16 +7,18 @@ using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
 
-namespace WebApplication1
+namespace EntertainmentHub
 {
-    public class MvcApplication : System.Web.HttpApplication
+    public class Global : System.Web.HttpApplication
     {
-        protected void Application_Start()
+
+        protected void Application_Start(object sender, EventArgs e)
         {
             AreaRegistration.RegisterAllAreas();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
         }
+
     }
 }
