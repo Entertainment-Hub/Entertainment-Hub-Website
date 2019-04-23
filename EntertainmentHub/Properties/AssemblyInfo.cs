2f6ba3d72b5d4f1fd1c59928885fef14a62286a0
BrianHwang888 <bh1531@nyu.edu>
2019-04-23 19:55:02 -0400

    Initial commit

--- EntertainmentHub/Properties/AssemblyInfo.cs
+++ EntertainmentHub/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("WebApplication1")]
+[assembly: AssemblyTitle("EntertainmentHub")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("WebApplication1")]
+[assembly: AssemblyProduct("EntertainmentHub")]
 [assembly: AssemblyCopyright("Copyright ©  2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("c400457b-e557-4db2-9f5f-a4bfe4334b48")]
+[assembly: Guid("658ca803-1a25-46d0-9859-74e28a57eefc")]
 
 // Version information for an assembly consists of the following four values:
 //
